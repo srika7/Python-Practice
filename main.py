3832b19eed07c1f5621dfaf8e0e41fe5734303e1
Srikanth <110478326+srika7@users.noreply.github.com>
2024-07-29 12:41:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# main.py
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+a = os.getenv('a')
+b = os.getenv('b')
+c = os.getenv('c')
+d = os.getenv('d')
+e = os.getenv('e')
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"a": a, "b": b, "c": c, "d": d, "e": e}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
